a67be29514bdb99a6bdda422e05cb8d1e8bb123c
Kyle Chadha <kyle.chadha@gmail.com>
2014-01-30 22:19:02 -0800

    Add status and input style

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var app = angular.module("Dapp", [])
 app.controller("DappCtrl", function($scope) {
-  
-
+  $scope.status = ["All good!", "Gandhi probably wouldn't...", "Danger! You's prolly fsckkued up~!"];
+  $scope.green = true;
+  $scope.yellow = false;
+  $scope.red = false;
 });
