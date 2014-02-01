340193131270b9582df8ddb496e037636ce3edce
Kyle Chadha <kyle.chadha@gmail.com>
2014-01-31 17:45:02 -0800

    Add firebase

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
-var app = angular.module("Dapp", [])
-app.controller("DappCtrl", function($scope) {
+var app = angular.module("Dapp", ["firebase"])
+app.controller("DappCtrl", function($scope, $firebase) {
+
+  dappRef = new Firebase("https://drinkmometer.firebaseio.com/");
+  $scope.fbRoot = $firebase(dappRef);
+
+  var IDs;
+  $scope.fbRoot.$on("loaded", function() {
+    IDs = $scope.fbRoot.$getIndex();
+
+    if (IDs.length == 0) {
+      $scope.fbRoot.$add( {
+        times: [''],
+        total: 0
+      });
+      $scope.fbRoot.$on("change", function() {
+        IDs = $scope.fbRoot.$getIndex();
+        $scope.db = $scope.fbRoot.$child(IDs[0]);
+      });
+    } else {
+      $scope.db = $scope.fbRoot.$child(IDs[0]);
+    };
+
+  });
+
   $scope.options = ["Have another drink!", "Gandhi wouldn't...", "You'sdeffsckked p"];
   $scope.status = $scope.options[0];
-  $scope.total = 0;
-  $scope.times = [{time: "12PM", drinks:["s", "b"]}, {time: "1PM", drinks:["b", "m", "s"]}, {time: "2PM", drinks:["m"]}];
+  // $scope.total = 0;
+  // $scope.times = [{time: "12PM", drinks:["s", "b"]}, {time: "1PM", drinks:["b", "m", "s"]}, {time: "2PM", drinks:["m"]}];
 
   $scope.drinkEvaluator = function(drink) {
 
@@ -12,40 +35,46 @@ app.controller("DappCtrl", function($scope) {
     var suffix = (hour < 12) ? "AM" : "PM";
     hour = ((hour + 11) % 12 + 1) + suffix;
 
-    for (i in $scope.times) {
-      if ($scope.times[i].time == hour) {
-        found = true;
-        $scope.times[i].drinks.push(drink);
-        break;
+    if ($scope.db.times[0] == '') {
+      $scope.db.times = [{time: hour, drinks: [drink]}]
+    } else {
+      for (i in $scope.db.times) {
+        if ($scope.db.times[i].time == hour) {
+          found = true;
+          $scope.db.times[i].drinks.push(drink);
+          break;
+        }
       }
-    }
 
-    if (!found) {
-      $scope.times.push({time: hour, drinks: [drink]});    
+      if (!found) {
+        $scope.db.times.push({time: hour, drinks: [drink]});
+      }
     }
 
     switch(drink)
     {
     case 'b':
-      $scope.total += 1;
-      console.log($scope.total);
+      $scope.db.total += 1;
+      console.log($scope.db.total);
       break;
     case 'm':
-      $scope.total += 1.5;
-      console.log($scope.total);
+      $scope.db.total += 1.5;
+      console.log($scope.db.total);
       break;
     case 's':
-      $scope.total += 2;
-      console.log($scope.total);
+      $scope.db.total += 2;
+      console.log($scope.db.total);
       break;
     }
 
-    if ($scope.total >= 10) {
+    if ($scope.db.total >= 15) {
       $scope.status = $scope.options[2];
     }
-    else if ($scope.total >= 5) {
+    else if ($scope.db.total >= 10) {
       $scope.status = $scope.options[1];
     }
+
+    $scope.db.$save();  
   }
 
 
