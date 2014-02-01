797b85b359340fe26d62c469d8ff1158482dda40
Kyle Chadha <kyle.chadha@gmail.com>
2014-01-31 16:48:23 -0800

    Add total functionality

--- js/main.js
+++ js/main.js
@@ -1,55 +1,52 @@
 var app = angular.module("Dapp", [])
 app.controller("DappCtrl", function($scope) {
-  $scope.status = ["All good!", "Not recommended...", "No! Don't do it!"];
-  $scope.green = true;
-  $scope.orange = false;
-  $scope.red = false;
+  $scope.options = ["Have another drink!", "Gandhi wouldn't...", "You'sdeffsckked p"];
+  $scope.status = $scope.options[0];
+  $scope.total = 0;
   $scope.times = [{time: "12PM", drinks:["s", "b"]}, {time: "1PM", drinks:["b", "m", "s"]}, {time: "2PM", drinks:["m"]}];
 
   $scope.drinkEvaluator = function(drink) {
 
-  /*
+    var found = false;
+    var hour = new Date().getHours();
+    var suffix = (hour < 12) ? "AM" : "PM";
+    hour = ((hour + 11) % 12 + 1) + suffix;
 
-  Inputs:
-  1. The time
-  2. How many drinks you want to take per hour
-  3. How many drinks total you want to take up to a given hour
+    for (i in $scope.times) {
+      if ($scope.times[i].time == hour) {
+        found = true;
+        $scope.times[i].drinks.push(drink);
+        break;
+      }
+    }
 
-  Calculations:
-  1. Per Hour Intake
-  9PM = (Qb * B * 1) + (Qm * M * 1.5) + (Qs * S * 2)*/
+    if (!found) {
+      $scope.times.push({time: hour, drinks: [drink]});    
+    }
 
-  var found = false;
-  var hour = new Date().getHours();
-  var suffix = (hour < 12) ? "AM" : "PM";
-  hour = ((hour + 11) % 12 + 1) + suffix;
-  console.log(hour);
-
-  for (i in $scope.times) {
-    if ($scope.times[i].time == hour) {
-      found = true;
-      $scope.times[i].drinks.push(drink);
+    switch(drink)
+    {
+    case 'b':
+      $scope.total += 1;
+      console.log($scope.total);
       break;
+    case 'm':
+      $scope.total += 1.5;
+      console.log($scope.total);
+      break;
+    case 's':
+      $scope.total += 2;
+      console.log($scope.total);
+      break;
+    }
+
+    if ($scope.total >= 10) {
+      $scope.status = $scope.options[2];
+    }
+    else if ($scope.total >= 5) {
+      $scope.status = $scope.options[1];
     }
   }
 
-  if (!found) {
-    $scope.times.push({time: hour, drinks: [drink]});    
-  }
 
-  console.log($scope.times)
-
-  /*
-  2. Total Intake
-
-  3a. Where you stand by hour
-
-  3b. Where you stand total
-
-
-  Outputs:
-
-  */
-
-  }
 });
